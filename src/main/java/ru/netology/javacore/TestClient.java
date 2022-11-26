9ecfeb0594520827f8cb53c573d102cc2abdc289
Andrey Iljasov <a_iljasov@mail.ru>
2022-11-26 23:30:36 +0300

    TestClient refactoring

--- src/main/java/ru/netology/javacore/TestClient.java
+++ src/main/java/ru/netology/javacore/TestClient.java
@@ -7,22 +7,19 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class TestClient {
     private static final int PORT = 8085;
     private static final String HOST = "localhost";
 
     public static void main(String[] args) throws IOException {
-        String stringJson = "";
-        List<String> list = Files.readAllLines(Path.of("order.txt"));
-        for (String order : list) {
+        var list = Files.readAllLines(Path.of("order.txt"));
+        for (var order : list) {
             try (Socket clientSocket = new Socket(HOST, PORT);
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(
+                 var out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 var in = new BufferedReader(new InputStreamReader(
                          clientSocket.getInputStream()))) {
-                stringJson = order;
-                out.println(stringJson);
+                out.println(order);
                 System.out.println(in.readLine());
             } catch (IOException e) {
                 throw new RuntimeException(e);
